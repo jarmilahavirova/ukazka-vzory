3c10f5ae5881294671111caabae239d10b4ff431
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-20 23:45:30 +0200

    Řešení úkolu 3

--- src/pages/ListPage/components/List/index.jsx
+++ src/pages/ListPage/components/List/index.jsx
@@ -1,28 +1,47 @@
-import { useEffect, useState } from 'react';
-import { ListItem } from '../ListItem';
+import { useEffect, useState } from "react";
+import { ListItem } from "../ListItem";
 
 export const List = () => {
   const [items, setItems] = useState(null);
+  const [fetchData, setFetchData] = useState("items");
 
   useEffect(() => {
     const fetchItems = async () => {
-      const response = await fetch('http://localhost:4000/api/items');
+      const response = await fetch(`http://localhost:4000/api/${fetchData} `);
       const data = await response.json();
       setItems(data.result);
     };
 
     fetchItems();
-  }, []);
+  }, [fetchData]);
 
   if (items === null) {
     return <p>Loading...</p>;
   }
 
   return (
-    <div className="list">
-      {items.map((item) => (
-        <ListItem key={item.id} item={item} />
-      ))}
-    </div>
+    <>
+      <div className="list">
+        {items.map((item) => (
+          <ListItem key={item.id} item={item} />
+        ))}
+      </div>
+      <select
+        onChange={(event) => {
+          setFetchData(event.target.value);
+        }}
+      >
+        <option value="items"> Všechny položky</option>
+        <option value="items?filter=nutrients.fiber.value:gt:2">
+          všechny položky s množstvím vlákniny větším než 2 g na 100 g
+        </option>
+        <option value="items?filter=nutrients.proteins.value:gt:1.5">
+          všechny položky s množstvím bílkovin větším než 1,5 g na 100 g
+        </option>
+        <option value="items?filter=nutrients.fats.value:lt:1.3">
+          všechny položky s množstvím tuků menším než 1,3 g na 100 g
+        </option>
+      </select>
+    </>
   );
 };
